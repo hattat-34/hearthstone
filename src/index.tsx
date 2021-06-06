456f572a50c19bba6ba23bf61bd33a031f668b73
nurullah.demir <nurullah.demir@gvg.com.tr>
2021-06-06 16:38:31 +0300

    Invention of the wheel.

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+const Hearthstone = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default Hearthstone
